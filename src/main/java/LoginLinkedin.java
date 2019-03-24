6a06fe6038ff6c421294ddad5b0c3593424def26
Yura Ultra <yura.ultra@gmail.com>
2019-03-24 17:01:12 +0200

    DZ 24.03.2019

---
+++ src/main/java/LoginLinkedin.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginLinkedin {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com");
+        WebElement login = driver.findElement(By.xpath("//input[@class='login-email reg-field__input']"));
+        login.sendKeys("yura.ultra@gmail.com");
+        WebElement password = driver.findElement(By.xpath("//input[@class='login-password reg-field__input']"));
+        password.sendKeys("UltraDaiver1");
+        password.sendKeys(Keys.ENTER);
+
+    }
+}
+
+
+//input[@class='login submit-button']
