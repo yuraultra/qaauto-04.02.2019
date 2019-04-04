f83f899cb359199f61af237abcf1eb3575280667
Yura Ultra <yura.ultra@gmail.com>
2019-04-04 21:42:48 +0300

    Lesson 6

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -1,34 +1,43 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LinkedinLoginTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com");
+        loginPage = new LoginPage(driver);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
     @Test
     public void successfulLoginTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com");
-
-        LoginPage loginPage = new LoginPage(driver);
         Assert.assertTrue(loginPage.isPageLoaded(),
                 "Login page was not loaded.");
-        loginPage.login("yura.ultra@gmail.com","Ultra_777");
+        loginPage.login("yura.ultra@gmail.com", "Ultra_777");
 
         HomePage homePage = new HomePage(driver);
         Assert.assertTrue(homePage.isPageLoaded(),
                 "Home page is not loaded.");
-
-        driver.quit();
     }
+
     @Test
     public void notSuccessfulLoginTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com");
-
-        LoginPage loginPage = new LoginPage(driver);
         Assert.assertTrue(loginPage.isPageLoaded(),
                 "Login page was not loaded.");
-        loginPage.login("yura.ultra@gmail.comxx","Ultra_777");
+        loginPage.login("yura.ultra@gmail.comxx", "Ultra_777");
 
         ErrorPage errorPage = new ErrorPage(driver);
         Assert.assertTrue(errorPage.isPageLoaded(),
@@ -39,21 +48,15 @@ public class LinkedinLoginTest {
                 "Hmm, that's not the right password. Please try again or request a new one.",
                 "userPassword validation message is incorrect.");
 
-        driver.quit();
     }
+
     @Test
     public void emptyFieldLoginPassword() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com");
-
-        LoginPage loginPage = new LoginPage(driver);
         Assert.assertTrue(loginPage.isPageLoaded(),
                 "Login page was not loaded.");
-        loginPage.login("yura.ultra@gmail.com","");
+        loginPage.login("yura.ultra@gmail.com", "");
 
         Assert.assertTrue(loginPage.isPageLoaded(),
                 "Login page was not loaded.");
-
-        driver.quit();
     }
 }
